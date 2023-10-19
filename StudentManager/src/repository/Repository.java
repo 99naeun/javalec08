a4d548b9cb736926ba784ed9205872e84b2831ad
99naeun <99naeun@naver.com>
2023-10-19 22:10:24 +0900

    Day19

---
+++ StudentManager/src/repository/Repository.java
@@ -0,0 +1,38 @@
+package repository;
+
+import java.util.List;
+import entity.Student;
+
+public interface Repository {
+	
+	//CRUD
+	//1. Read(Select)
+	//학생목록 전체 들고오기
+	List<Student> selectStudents() throws Exception;
+	
+	//학생정보 하나 불러오기(학번 기준으로 Where 해와라)
+	Student SelectStudent(int stdNo) throws Exception;
+	
+	//학생이름으로 학생정보 불러오기
+	//동명이인이 존재할 수 있으니 
+	List<Student> selectStudent(String name) throws Exception;
+	
+	//최종 추가된 학생 하나 불러오기
+	Student lastInsertedStudent() throws Exception;
+
+	//2.Create(Insert)
+	//학생 입력하기
+	boolean registStudent(Student student) throws Exception;
+	
+	//3.Update(update)
+	//학생 한명 수정하기
+	int updateStudent(int stdNo, Student student) throws Exception;
+	
+	//4.Delete(Delete)
+	//학생 한명 삭제하기
+	boolean deleteStudent(int stdNo) throws Exception;
+	
+	//5.자원반납(DB Close)
+	void closeRepository() throws Exception;
+	
+}
